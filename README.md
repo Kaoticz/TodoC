6629c63c01f4e4c8d7c77cce8b5dada884389fbf
Kaoticz <1812311+Kaoticz@users.noreply.github.com>
2023-11-13 20:20:58 -0300

    Small signal demo

---
+++ README.md
@@ -0,0 +1,3 @@
+# TodoC
+
+University assignment of a simple "To Do" program written in C.
--- main.c
+++ main.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
+#include <signal.h>
 
+void sigtstp_handler(int);
+
+/// @brief The entry point of the application.
+/// @return The exit code of the program.
 int main()
 {
-    printf("Hello world!\n");
+    // Set the handler for SIGTSTP (Ctrl-Z).
+    signal(SIGTSTP, sigtstp_handler);
+    getchar();
+
     return 0;
+}
+
+/// @brief Executes an action when Ctrl + Z is pressed.
+/// @param sigtstp_num The signal number for SIGTSTP.
+void sigtstp_handler(int sigtstp_num)
+{
+    printf("Cannot execute Ctrl+Z\n");
+
+    // Reset handler to catch SIGTSTP next time
+    signal(SIGTSTP, sigtstp_handler);
 }
